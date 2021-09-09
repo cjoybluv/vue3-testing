0ec9637b39abaa40bd8c227faf20e77324956a6c
Dave Hemmer <dave_hemmer@hotmail.com>
2021-09-09 12:31:18 -0700

    clean & fetching to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,42 @@
 import { createStore } from "vuex";
+import axios from "axios";
+
+interface INation {
+  "ID Nation": string;
+  "ID Year": number;
+  Nation: string;
+  Population: number;
+  "Slug Nation": string;
+  Year: string;
+}
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    nations: [],
+  },
+  getters: {
+    nations(state) {
+      return state.nations;
+    },
+  },
+  mutations: {
+    SET_NATIONS(state, nations) {
+      state.nations = nations;
+    },
+  },
+  actions: {
+    fetchNations({ commit }) {
+      return new Promise((resolve) => {
+        axios
+          .get(
+            "https://datausa.io/api/data?drilldowns=Nation&measures=Population"
+          )
+          .then((response: { data: { data: INation[]; source: unknown } }) => {
+            commit("SET_NATIONS", response.data.data);
+            resolve(response.data);
+          });
+      });
+    },
+  },
   modules: {},
 });
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul>
+      <div v-for="nation in nations" :key="nation['ID NATION']">
+        {{ nation.Nation }} - {{ nation.Population }} - {{ nation.Year }}
+      </div>
+    </ul>
   </div>
 </template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+import { mapActions, useStore } from "vuex";
+
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    const nations = computed(() => store.getters["nations"]);
+
+    return { nations };
+  },
+  mounted() {
+    this.fetchNations();
+  },
+  methods: {
+    ...mapActions({
+      fetchNations: "fetchNations",
+    }),
+  },
+});
+</script>
